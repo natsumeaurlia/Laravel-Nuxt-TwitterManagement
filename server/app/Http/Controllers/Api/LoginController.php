1b4487db3017143b012f9d0078d7156b75d0279e
natsume0718 <moto718program@gmail.com>
2022-01-29 01:30:49 +0900

    リクエストとコントローラー作成

---
+++ server/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginRequest $request)
+    {
+    }
+}
