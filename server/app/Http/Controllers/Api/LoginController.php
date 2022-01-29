3da8508401f05ff81379c20285374850cb1b8fed
natsume0718 <moto718program@gmail.com>
2022-01-29 23:36:10 +0900

    Actionを消す

--- server/app/Http/Controllers/Api/LoginController.php
+++ server/app/Http/Controllers/Api/LoginController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
 use App\Http\Resources\LoginResource;
-use App\UseCases\User\LoginAction;
+use App\UseCases\User\Login;
 
 class LoginController extends Controller
 {
-    public function __invoke(LoginRequest $request, LoginAction $login)
+    public function __invoke(LoginRequest $request, Login $login)
     {
         if ($user = $login->handle($request->email, $request->password)) {
             return new LoginResource($user);
--- server/app/Http/Controllers/Api/RegisterController.php
+++ server/app/Http/Controllers/Api/RegisterController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Http\Resources\RegisterResource;
-use App\UseCases\User\StoreAction;
+use App\UseCases\User\Store;
 
 class RegisterController extends Controller
 {
-    public function __invoke(RegisterRequest $request, StoreAction $store)
+    public function __invoke(RegisterRequest $request, Store $store)
     {
         $user = $store->handle($request->name, $request->email, $request->password);
 
