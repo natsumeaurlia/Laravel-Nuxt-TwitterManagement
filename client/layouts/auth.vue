bc6375cd6bcc10f9ab3c789fd8f909b6f925cc55
natsume0718 <moto718program@gmail.com>
2022-02-05 15:44:19 +0900

    ログイン画面作成

---
+++ client/layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-main style="background:#F5F5F5;">
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+</template>
---
+++ client/pages/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card
+      class="mx-auto fill-width"
+      flat
+      max-width="640"
+  >
+    <v-card-title class="text-center pa-8">
+      <h4 class="fill-width">ログイン</h4>
+    </v-card-title>
+    <v-divider></v-divider>
+    <div class="px-6 py-8">
+      <div style="max-width:344px" class="mx-auto">
+        <div class="pt-6">
+          <div>
+            <input-email />
+            <input-password />
+          </div>
+          <div class="login-btn pb-8">
+            <v-btn
+                class="fill-width caption"
+                color="#FFCB00"
+                depressed
+                height="48px"
+                tile
+            >
+              ログイン
+            </v-btn>
+          </div>
+          <v-divider></v-divider>
+          <div class="pt-8 pb-4">
+            <span>まだアカウントをお持ちではないですか？</span>
+            <nuxt-link to="/login">新規登録に移動</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+
+definePageMeta({
+  layout: 'auth'
+})
+
+</script>
+
+<style scoped>
+
+</style>
