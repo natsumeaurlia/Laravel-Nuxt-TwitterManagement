d94a3f5251c31b2d46592b771978139a94738bd1
natsume0718 <moto718program@gmail.com>
2022-02-05 15:42:15 +0900

    vuetifyとicon導入

--- client/plugins/vuetify.ts
+++ client/plugins/vuetify.ts
@@ -1,5 +1,7 @@
 import { createVuetify } from "vuetify";
 import * as Components from 'vuetify/components'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({components: Components});
