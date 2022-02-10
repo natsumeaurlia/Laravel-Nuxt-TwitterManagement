1eb6854916b78e671af9a31900e5fa97a82b7b63
natsumeaurlia <moto718program@gmail.com>
2022-02-11 01:56:28 +0900

    eslint修正

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   // add your custom rules here
   rules: {
     "object-curly-spacing": ["error", "always"],
-    "multi-word-component-names": 0
+    'vue/multi-word-component-names': 'off'
   }
 };
