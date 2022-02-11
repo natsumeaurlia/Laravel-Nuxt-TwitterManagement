57ef10da7843aa4bbad43a9daaa5ee8eceb46f5f
natsumeaurlia <moto718program@gmail.com>
2022-02-12 02:52:10 +0900

    サイドバー追加(todo: TS動的アイコン)

---
+++ client/layouts/authenticated.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <sidebar :items="[{icon: 'mdi-speedometer',name: 'ダッシュボード'},{icon: 'mdi-account',name: 'アカウント管理'}]"/>
+    <v-main style="background-color:#F5F5F5;" class="fill-height">
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import Sidebar from "~/components/dashboard/sidebar.vue";
+
+export default defineComponent({
+  components: { Sidebar }
+})
+
+</script>
--- client/pages/dashboard.vue
+++ client/pages/dashboard.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  layout: 'authenticated'
+});
 </script>
 
 <style scoped>
