362fe16292b5f69395f307767143b4fc2afd85ef
natsumeaurlia <moto718program@gmail.com>
2022-02-15 02:58:41 +0900

    idはtwitterアカウントidのため外部で使っても問題ないため、uuid削除

--- server/app/Http/Resources/AccountResource.php
+++ server/app/Http/Resources/AccountResource.php
@@ -9,7 +9,7 @@ class AccountResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'uuid' => $this->uuid,
+            'id' => $this->id,
             'name' => $this->name,
             'screen_name' => $this->screen_name,
             'avatar_path' => $this->avatar_path,
