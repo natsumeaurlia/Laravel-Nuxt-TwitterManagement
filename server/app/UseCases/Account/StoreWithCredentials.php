e364aed955ebdacc0c7fb87ccacb783a925fac51
natsumeaurlia <moto718program@gmail.com>
2022-02-16 01:09:45 +0900

    自身の認証キーからの保存処理作成

---
+++ server/app/UseCases/Account/StoreWithCredentials.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\UseCases\Account;
+
+use App\Models\User;
+use App\Services\TwitterApiService;
+use App\UseCases\Account\Exception\MissingCredentialException;
+
+class StoreWithCredentials
+{
+    /**
+     * @return User
+     * @throws MissingCredentialException
+     */
+    public function __invoke(User $user, string $accessToken, string $accessTokenSecret, string $consumerKey, string $consumerSecret)
+    {
+        $twitter = TwitterApiService::createWithCredentials($accessToken, $accessTokenSecret, $consumerKey, $consumerSecret);
+        $twitterUser = $twitter->getCredentials();
+        if (!$twitterUser) {
+            throw new MissingCredentialException('missing credential.', 403);
+        };
+
+        return $user->accounts()->create([
+            'id' => $twitterUser->id,
+            'name' => $twitterUser->name,
+            'screen_name' => $twitterUser->screen_name,
+            'avatar_path' => $twitterUser->profile_image_url,
+            'access_token' => $accessToken,
+            'access_token_secret' => $accessTokenSecret,
+            'api_key' => $consumerKey,
+            'api_secret_key' => $consumerSecret
+        ]);
+
+    }
+}
