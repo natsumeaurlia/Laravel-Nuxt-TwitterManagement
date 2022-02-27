e00b60ca6cb57b77fbe45bf04fa4d9a48e414679
natsumeaurlia <moto718program@gmail.com>
2022-02-28 01:31:42 +0900

    型をTSに変更

--- client/repositories/userRepository.ts
+++ client/repositories/userRepository.ts
@@ -4,7 +4,7 @@ import { NuxtAxiosInstance } from '@nuxtjs/axios'
 export class UserRepository {
   constructor(private readonly axios: NuxtAxiosInstance) {}
 
-  public store(name: string, email: string, password: string) {
+  public store(name: String, email: String, password: String) {
     return this.axios.post('/api/auth/register', { name, email, password })
   }
 }
