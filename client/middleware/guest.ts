2c2c2d2e3141e20eb5e85e1c789656435f362b0f
natsumeaurlia <moto718program@gmail.com>
2022-03-01 00:28:15 +0900

    変数名変更

--- client/middleware/guest.ts
+++ client/middleware/guest.ts
@@ -2,10 +2,10 @@ import { Middleware, Context } from '@nuxt/types'
 
 const redirectTo = '/'
 
-const myMiddleware: Middleware = (context: Context) => {
+const onlyGuest: Middleware = (context: Context) => {
   if (context.app.$auth.$state.loggedIn) {
     return context.redirect(redirectTo)
   }
 }
 
-export default myMiddleware
+export default onlyGuest
