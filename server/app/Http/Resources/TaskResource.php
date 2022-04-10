6d4b9d5b60e4ae70e0e4f285940238d05e5cd54e
natsumeaurlia <moto718program@gmail.com>
2022-04-11 00:21:31 +0900

    account情報追加

--- server/app/Http/Resources/TaskResource.php
+++ server/app/Http/Resources/TaskResource.php
@@ -18,6 +18,7 @@ class TaskResource extends JsonResource
             'range_min_sleep_time' => $this->range_min_sleep_time,
             'type' => $this->type,
             'is_enable' => $this->is_enable,
+            'account' => new AccountResource($this->account),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
