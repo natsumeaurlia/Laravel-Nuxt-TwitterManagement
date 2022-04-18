959030aa749bebb264fe4b585897190c2a7e3181
natsumeaurlia <moto718program@gmail.com>
2022-04-19 01:31:04 +0900

    タスクindexの空作成

---
+++ client/pages/task/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+      <h1>定期タスク管理</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, } from '@nuxtjs/composition-api'
+
+
+export default defineComponent({
+  components: {},
+  layout: 'authenticated',
+  setup() {
+
+  },
+})
+</script>
+
+<style scoped></style>
