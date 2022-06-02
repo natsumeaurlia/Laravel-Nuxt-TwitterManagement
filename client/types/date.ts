44c53bfdce5b4713954719f8d75ec3fd787be6ce
natsumeaurlia <moto718program@gmail.com>
2022-06-03 01:16:37 +0900

    lint fix

--- client/types/date.ts
+++ client/types/date.ts
@@ -1,17 +1,17 @@
-type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type OneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 /**
  * Years
  */
-type YYYY = `19${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`
+type YYYY = `19${OneToNine}${ZeroToNine}` | `20${ZeroToNine}${ZeroToNine}`
 /**
  * Months
  */
-type MM = `0${oneToNine}` | `1${0 | 1 | 2}`
+type MM = `0${OneToNine}` | `1${0 | 1 | 2}`
 /**
  * Days
  */
-type DD = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`
+type DD = `${0}${OneToNine}` | `${1 | 2}${ZeroToNine}` | `3${0 | 1}`
 /**
  * YYYY-MM-DD
  */
