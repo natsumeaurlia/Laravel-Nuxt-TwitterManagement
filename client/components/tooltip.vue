f863350184ce6e2e3a18df1ff8ec7c26f50e8175
natsumeaurlia <moto718program@gmail.com>
2022-06-15 17:04:03 +0900

    ツーリチップコンポーネント作成

---
+++ client/components/tooltip.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip top :open-on-hover="true" :open-on-click="false">
+    <template #activator="{ on, attrs }">
+      <v-btn icon v-bind="attrs" v-on="on">
+        <v-icon>mdi-help-circle</v-icon>
+      </v-btn>
+    </template>
+    <slot />
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "Tooltip"
+})
+</script>
+
+<style scoped>
+
+</style>
