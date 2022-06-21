75ace40db08effb4757b078207d1e13f60023a0d
natsumeaurlia <moto718program@gmail.com>
2022-06-22 02:37:21 +0900

    タスクAPIにプロパティ追加漏れ

--- client/types/task.ts
+++ client/types/task.ts
@@ -10,7 +10,9 @@ export interface Task {
   start_time_period: Hi
   end_time_period: Hi
   max_execution: number
+  execution_interval: number
   range_min_sleep_time: number
+  range_max_sleep_time: number
   type: TaskType
   is_enable: boolean
   account: Account
@@ -18,7 +20,7 @@ export interface Task {
   updated_at: string
 }
 
-export interface CreateTask {
+export interface TaskForm {
   taskName: string
   selectedAction: TaskType | ''
   selectedAccount: string | object
